e99733bcc786141cb623c0e4de1d5a4fdb236a3e
ayobpro <rockkapak99@gmail.com>
2022-10-06 21:26:39 +0800

    Latest esx legacy used this

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,4 +4,4 @@ description 'PEFCL framework integration for ESX'
 author "Project Error"
 lua54 'yes'
 
-server_scripts { 'config.lua', 'server.lua' }
+server_scripts { '@es_extended/imports.lua', 'config.lua', 'server.lua' }
